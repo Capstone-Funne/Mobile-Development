27fd600ba4025edae61fa7fd2abcb2e0307ab200
wirabudhi <bpantaiselatan@gmail.com>
2023-06-06 23:31:31 +0800

    Final Authentication

--- app/src/main/java/com/example/funne/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/funne/ui/activity/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.funne.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.funne.R
+import com.example.funne.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = binding.bottomNavigation
+        setupWithNavController(bottomNavigationView, navController)
     }
 }
